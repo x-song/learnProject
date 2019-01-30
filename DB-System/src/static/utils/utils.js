1647cae2af3286686a7584a8179c97c713049da3
x-song <374998683@qq.com>
2019-01-30 18:50:56 +0800

    ---

--- DB-System/src/static/utils/utils.js
+++ DB-System/src/static/utils/utils.js
@@ -1,7 +1,6 @@
 import  module from '../../module'
 export  default  {
    getMenu(){
-     //test
-     return module.menuList.map(item=>item.title)
+      return module.menuList.map(item=>item.title)
    }
 }
